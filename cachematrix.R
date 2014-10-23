c11a15efa7be0470a5d7e04c8e1cb4a887991a09
Angeles F <m.angeles.flores@gmail.com>
2014-10-23 21:02:57 +0200

    Assignment2 developed
    A pair of functions that creates a special type of matrix, calculate
    its inverse and caches it to reuse it.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that creates a special type of matrix that can save its inverse 
+## and calculates the inverse of the matrix.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(theMatrix = matrix()) {
+    
+    theMean <- NULL
+    
+    set <- function(matrixToCalc) {
+        theMatrix <<- matrixToCalc
+        theMean <<- NULL
+    }
+    
+    get <- function() {
+        theMatrix
+    }
+    
+    setmean <- function(meanToSet) {
+        theMean <<- meanToSet
+    }
+    
+    getmean <- function(){
+        theMean  
+    } 
+    
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+## In case the inverse is already calculated, it returns the cached inverse
+cacheSolve <- function(theMatrix, ...) {
+    theMean <- theMatrix$getmean()
+    if(!is.null(theMean)) {
+        message("the mean is already calculated and the cache will be returned")
+        return(theMean)
+    }
+    data <- theMatrix$get()
+    theMean <- solve(data, ...)
+    theMatrix$setmean(theMean)
+    theMean
 }
