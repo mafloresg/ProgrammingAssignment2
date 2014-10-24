346097be1c69ecd84c36708407a8cbc0da976d01
Angeles F <m.angeles.flores@gmail.com>
2014-10-24 19:21:49 +0200

    Added more comments
    Some comments were added and changed one variable name

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,17 @@
 ## A pair of functions that creates a special type of matrix, calculate
-## its inverse and caches it to reuse it.
-
-## This function creates a special "matrix" object that can cache its inverse.
-## theMatrix is a square matrix (NxN)
-## Returns an object with the matrix and some functions used to set and get the data and the mean
+## its inverse and saves it in a "cache" in order to reuse it when is needed 
+## instead of calculate it again.
 
 makeCacheMatrix <- function(theMatrix = matrix()) {
+
+    ## This function creates a special matrix object that can cache its inverse.
+    ## theMatrix must be a square matrix (size NxN)
+    ## Returns an object that contains the matrix and some functions used to set and get the data and the inverse
+    
+    ## set saves the original matrix and resets the inverse matrix to NULL
+    ## get returns the original matrix
+    ## setInverse saves the inverse matrix
+    ## getInverse returns the inverse matrix
     
     theInverse <- NULL
     
@@ -18,8 +24,8 @@ makeCacheMatrix <- function(theMatrix = matrix()) {
         theMatrix
     }
     
-    setInverse <- function(meanToSet) {
-        theInverse <<- meanToSet
+    setInverse <- function(inverseToSave) {
+        theInverse <<- inverseToSave
     }
     
     getInverse <- function(){
@@ -32,12 +38,13 @@ makeCacheMatrix <- function(theMatrix = matrix()) {
 }
 
 
-## Return a matrix that is the inverse of 'x'
-## In case the inverse is already calculated, it returns the cached inverse
 cacheSolve <- function(theMatrix, ...) {
+    ## This function returns the inverse of the matrix encapsulated in a makeCacheMatrix object
+    ## In case the inverse is already calculated, it returns the cached inverse instead of calculate it again
+    
     theInverse <- theMatrix$getInverse()
     if(!is.null(theInverse)) {
-        message("the inverse is already calculated and the cache will be returned")
+        message("the inverse is already calculated and the cached value will be returned")
         return(theInverse)
     }
     data <- theMatrix$get()
